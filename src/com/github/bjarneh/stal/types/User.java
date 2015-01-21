0d55c22e366b10e4a298817a97e037e35e55c2ac
Bjarne Holen <bjarneholen@gmail.com>
2015-01-21 18:16:52 +0100

    forgot to include User object

---
+++ src/com/github/bjarneh/stal/types/User.java
@@ -0,0 +1,16 @@
+// Copyright 2015 bjarneh@ifi.uio.no. All rights reserved. 
+// Use of this source code is governed by a BSD-style 
+// license that can be found in the LICENSE file. 
+
+package com.github.bjarneh.stal.types;
+
+public class User {
+
+    public String id;
+    public String name;
+
+    @Override
+    public String toString(){
+        return String.format("User{%s, %s}", id, name);
+    }
+}
